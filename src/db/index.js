a02d4b311e348b2c4e933e574b4381bce0e82c31
AliHaider6812 <ah351036812@gmail.com>
2026-07-01 00:46:15 +0500

    Add subscription model

--- src/db/index.js
+++ src/db/index.js
@@ -4,15 +4,14 @@ import { DB_NAME } from "../constants.js";
 
 
 
-
-const connectionDB= async ()=>{
+const connectionDB = async () => {
     try {
-       const connectionInstance = await mongoose.connect
-       (`${process.env.MONGODB_URI}/${DB_NAME}`)
-       console.log(`\n MongoDB connected by malik ...!!DB host: 
+        const connectionInstance = await mongoose.connect
+            (`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected by malik ...!!DB host: 
         ${connectionInstance.connection.host}`);
-    } catch(error){
-        console.log("MONGODB connection error",error);
+    } catch (error) {
+        console.log("MONGODB connection error", error);
         process.exit(1)
     }
 }
